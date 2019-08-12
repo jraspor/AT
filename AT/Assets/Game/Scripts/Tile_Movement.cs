44103fc4e217bf7d5259d7556d344130afc10a4a
jraspor <37303480+jraspor@users.noreply.github.com>
2019-08-12 19:34:16 +0200

    Revert "Merge branch 'master' of https://github.com/jraspor/AT"
    This reverts commit e9263f24e384630f3ee9d92374f23c71fa899005, reversing
    changes made to dc65a61e6d29e2edff51cb8a0ce1a4156fe426b9.

---
+++ AT/Assets/Game/Scripts/Tile_Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_Movement : MonoBehaviour{
+
+    public float speed;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}

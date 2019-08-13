ec92b7026de29521d9c2bf1d1286171e175a98dd
jraspor <37303480+jraspor@users.noreply.github.com>
2019-08-13 16:20:08 +0200

    Add funcionality: quit

--- AT/Assets/MainMenu.cs
+++ AT/Assets/MainMenu.cs
@@ -8,17 +8,23 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
     }
 }

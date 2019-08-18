6768a50078da5ad90f939a7d797dcd389e611152
jraspor <37303480+jraspor@users.noreply.github.com>
2019-08-18 13:42:56 +0200

    Volume off

--- AT/Assets/Settings.cs
+++ AT/Assets/Settings.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider slider;
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SoundOff()
+    {
+        GameObject temp = GameObject.Find("Volume Slider");
+        audioMixer.SetFloat("volume", -50);
+        slider = temp.GetComponent<Slider>();
+        slider.value = -50;
     }
 }
